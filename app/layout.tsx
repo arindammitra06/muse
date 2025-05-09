53696013cdd28db5dc04e855604eafae9171b338
Arindam Mitra <arindammitra06@users.noreply.github.com>
2025-05-09 14:07:17 -0400

    Fix PWA 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en" {...mantineHtmlProps} className={`${inter.variable}`}>
       <head>
         <ColorSchemeScript />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta
           name="viewport"
